d201e817b9688c692bd50ec2f8b0206c524a389a
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-12-09 22:46:48 -0300

    chore: create mocks repository

---
+++ src/mocks/repositories.mock.ts
@@ -0,0 +1,22 @@
+export const repositories = [
+  {
+    id: '2',
+    title: 'some-title',
+    image: 'https://avatars.githubusercontent.com/u/29661994?v=4',
+    stars: 4,
+  },
+
+  {
+    id: '1',
+    title: 'some-title-2',
+    image: 'https://avatars.githubusercontent.com/u/29661994?v=4',
+    stars: 2,
+  },
+
+  {
+    id: '3',
+    title: 'some-title-3',
+    image: 'https://avatars.githubusercontent.com/u/29661994?v=4',
+    stars: 11,
+  },
+];
---
+++ src/mocks/repository.mock.ts
@@ -0,0 +1,5 @@
+export const mock = {
+  title: 'some-title',
+  image: 'https://avatars.githubusercontent.com/u/29661994?v=4',
+  stars: 4,
+};
