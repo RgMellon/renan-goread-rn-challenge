ec0118bbdd6d7c9fe5e5d78d577bb49eb299d080
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-12-10 19:05:26 -0300

    refactor: using the right params

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -11,15 +11,17 @@ type CardProps = {
 export function Card({ data, ...rest }: CardProps) {
   return (
     <S.Container testID="card-container" {...rest}>
-      <S.Avatar testID="card-avatar" source={{ uri: data.image }} />
+      <S.Avatar testID="card-avatar" source={{ uri: data.owner.avatar_url }} />
 
       <S.Content>
         <S.Info>
           <S.RepoInfo>
-            <S.Title>{data.title}</S.Title>
-            <S.Stars testID="card-stars">{data.stars} stars </S.Stars>
+            <S.Title>{data.name}</S.Title>
+            <S.Stars testID="card-stars">
+              {data.stargazers_count} stars{' '}
+            </S.Stars>
           </S.RepoInfo>
-          <S.User>{data.user}</S.User>
+          <S.User>{data.owner.login}</S.User>
         </S.Info>
       </S.Content>
     </S.Container>
